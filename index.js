c340f1cb40df971139c0b790910c8acb85dd0412
Julieta <julietallamazares96@gmail.com>
2024-06-09 21:49:13 -0300

    agrego el seed de jugadores pero falta el file env

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import conexion from "./conexion/conexion.js";
 import {
  Jugadores, Puntajes
 } from './Models/index.js';
+import jugoresSeed from "./seed/jugadoresSeed.js";
 
 
 const app = express();
@@ -13,9 +14,8 @@ const app = express();
 
 
 
-await conexion.sync({
-    force: true
-})
+await conexion.sync({force: false});
+await jugoresSeed()
 
 app.listen(8080, () => {
     console.log("server Ok");
